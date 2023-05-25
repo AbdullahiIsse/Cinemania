700cb6fcb1ba404fc5d06765f492fc0b612210a7
lennartsargezian <Lennart_1997@hotmail.com>
2023-05-26 01:57:15 +0200

    Test for Userservice done

--- src/test/java/com/sep/cinemania/Service/User/UserServiceTest.java
+++ src/test/java/com/sep/cinemania/Service/User/UserServiceTest.java
@@ -1,73 +1,110 @@
-//package com.sep.cinemania.Service.User;
-//
-//import com.sep.cinemania.Dao.User.UserRepository;
-//import com.sep.cinemania.Entities.User;
-//import com.sep.cinemania.Exception.User.UserAlreadyExistException;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import static org.junit.Assert.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertThrows;
-//import static org.mockito.Mockito.*;
-//
-//
-//import org.mockito.junit.MockitoJUnitRunner;
-//
-//
-//
-//import java.util.List;
-//
-//@RunWith(MockitoJUnitRunner.class)
-//public class UserServiceTest {
-//
-//    @InjectMocks
-//    private UserServiceImpl userService;
-//
-//    @Mock(strictness = Mock.Strictness.LENIENT)
-//    private UserRepository userRepository;
-//
-//
-//    @Test
-//    public void getUserList_should_returnTheUserList(){
-//        when(userRepository.findAll()).thenReturn(List.of(new User("were232","bob","bob@gmail.com")
-//                ,new User("gfdg3434","joe","joe@gmail.com"),
-//                 new User("fsds221","tim","tim@gmail.com")));
-//
-//        assertEquals(3,userService.getUserList().size());
-//    }
-//
-//
-//    @Test
-//    public void findUserByEmail_should_findTheUserByEmail(){
-//        var email = "tim@gmail.com";
-//        var user = new User("fsds221","tim","tim@gmail.com");
-//
-//        when(userRepository.findUserByEmail(email)).thenReturn(user);
-//
-//        assertEquals(user,userService.findUserByEmail(email));
-//    }
-//
-//    @Test
-//    public void createUser_should_succeed_when_UserDoNotAlreadyExist(){
-//        var user = new User("fsds2221","tim","tim1@gmail.com");
-//        when(userRepository.findUserByEmail(user.getEmail())).thenReturn(null);
-//
-//        when(userRepository.save(user)).thenReturn(user);
-//        User user1 = userService.createUser(user);
-//        when(user1).thenReturn(user);
-//
-//        assertEquals(user1.getId(),user.getId());
-//    }
-//
-//    @Test
-//    public void createUser_should_fail_when_UserAlreadyExist(){
-//        var user = new User("fsds2221","tim","tim1@gmail.com");
-//        when(userRepository.findUserByEmail(user.getEmail())).thenReturn(user);
-//
-//        assertThrows(UserAlreadyExistException.class, () -> userService.createUser(user));
-//    }
-//
-//
-//
-//}
+package com.sep.cinemania.Service.User;
+import com.sep.cinemania.Dao.User.UserRepository;
+import com.sep.cinemania.Entities.User;
+import com.sep.cinemania.Exception.User.UserAlreadyExistException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
+import org.mockito.junit.MockitoJUnitRunner;
+import java.util.List;
+import java.util.Optional;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserServiceTest {
+
+    @InjectMocks
+    private UserServiceImpl userService;
+
+    @Mock(strictness = Mock.Strictness.LENIENT)
+    private UserRepository userRepository;
+
+    //succes
+    @Test
+    public void getUserList_should_returnTheUserList(){
+        var user = new User("User1","tim","tim@gmail.com");
+        when(userRepository.findOtherUsersById(user.getId())).thenReturn(List.of(new User("were232","bob","bob@gmail.com")
+                ,new User("gfdg3434","joe","joe@gmail.com"),
+                 new User("fsds221","tim","tim@gmail.com")));
+
+        assertEquals(3,userService.findOtherUsersById(user.getId()).size());
+    }
+
+    //failed
+    @Test
+    public void getUserList_returnAnEmptyList() {
+        var user = new User("1", "joe", "joe@gmail.com");
+        when(userRepository.findOtherUsersById(user.getId())).thenReturn(null);
+        List<User> userList = userService.findOtherUsersById(user.getId());
+        assertNull(userList);
+    }
+
+    //succes
+    @Test
+    public void findUserByEmail_should_findTheUserByEmail(){
+        var user = new User("fsds221","tim","tim@gmail.com");
+
+        when(userRepository.findUserByEmail(user.getEmail())).thenReturn(user);
+
+        assertEquals(user,userService.findUserByEmail(user.getEmail()));
+    }
+
+    //fail
+    @Test
+    public void fail_findUserByEmail(){
+        var user = new User("fsds221","tim","tim@gmail.com");
+
+        when(userRepository.findUserByEmail(user.getEmail())).thenReturn(null);
+
+        assertNull(user.getEmail(),userService.findUserByEmail(user.getEmail()));
+    }
+
+
+    //succes
+    @Test
+    public void createUser_should_succeed_when_UserDoNotAlreadyExist(){
+        var user = new User("fsds2221","tim","tim1@gmail.com");
+        when(userRepository.findUserByEmail(user.getEmail())).thenReturn(null);
+
+        when(userRepository.save(user)).thenReturn(user);
+        User user1 = userService.createUser(user);
+        when(user1).thenReturn(user);
+
+        assertEquals(user1.getId(),user.getId());
+    }
+
+
+    @Test
+    public void findUserById() {
+        var user = new User("were232", "tim", "tim@gmail.com");
+
+        when(userService.findUserById(user.getId())).thenReturn(Optional.of(new User("were232", "bob", "bob@gmail.com")));
+
+        Optional<User> actualUser = userService.findUserById(user.getId());
+        assertTrue(actualUser.isPresent());
+
+        String actualUserId = actualUser.get().getId();
+        assertEquals(user.getId(), actualUserId);
+    }
+
+
+    @Test
+    public void failFindingUserById() {
+        var user = new User("were232", "tim", "tim@gmail.com");
+
+        when(userService.findUserById(user.getId())).thenReturn(Optional.empty());
+
+        Optional<User> actualUser = userService.findUserById(user.getId());
+        assertFalse(actualUser.isPresent());
+    }
+
+
+
+
+
+}
