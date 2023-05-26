c1c6a922c5e766b7d0a9bad3364208c219ac12dd
lennartsargezian <Lennart_1997@hotmail.com>
2023-05-26 02:30:40 +0200

    Test for MovieService Done

--- src/main/java/com/sep/cinemania/Entities/Movie.java
+++ src/main/java/com/sep/cinemania/Entities/Movie.java
@@ -39,6 +39,7 @@ public class Movie {
     @NotBlank(message = "image is mandatory")
     private String image;
 
+
     public Movie(long id, String title, String releaseDate, double voteAverage, String image) {
         this.id = id;
         this.title = title;
--- src/test/java/com/sep/cinemania/Service/Movie/MovieServiceTest.java
+++ src/test/java/com/sep/cinemania/Service/Movie/MovieServiceTest.java
@@ -12,6 +12,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -23,14 +24,28 @@ public class MovieServiceTest {
     @Mock(strictness = Mock.Strictness.LENIENT)
     private MovieRepository movieRepository;
 
-/*    @Test
+    //success
+   @Test
     public void findMoviesByUserId(){
-        var movie = new Movie(840326,"avatar","2023-01-27",8.7, "/pnQYjDxqZ3C6reI8N0MfNi4NMkZ.jpg");
-        when(movieRepository.findMoviesByUserId(movie.getUserId()).thenReturn(List.of(new User("were232","bob","bob@gmail.com")
-                ,new User("gfdg3434","joe","joe@gmail.com"),
-                new User("fsds221","tim","tim@gmail.com")));
+       var user = new User("User1","tim","tim@gmail.com");
+        when(movieRepository.findMoviesByUserId(user.getId())).thenReturn(List.of(new Movie(234,"avatar","2023-01-27",8.7, "/pnQYjZ.jpg")
+                ,new Movie(840332,"home","2023-01-28",8.7, "/pnQsdfjZ.jpg"),
+                new Movie(840777,"tesla","2023-01-29",8.7, "/pnQYsdfZ.jpg")));
 
-        assertEquals(3,userService.findOtherUsersById(user.getId()).size());
-    }*/
+        assertEquals(3,movieService.findMoviesByUserId(user.getId()).size());
+    }
+
+
+    @Test
+    public void failfindMoviesByUserId() {
+        var user = new User("1", "joe", "joe@gmail.com");
+        when(movieRepository.findMoviesByUserId(user.getId())).thenReturn(null);
+        List<Movie> movieList = movieService.findMoviesByUserId(user.getId());
+        assertNull(movieList);
+    }
+
+
+
+
 
 }
